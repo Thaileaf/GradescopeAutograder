0c47b349e271660a5050bf0464a414759322f4f2
Thaileaf <lethaison2019@gmail.com>
2023-09-07 23:17:32 -0400

    Refined tests

--- tests/catch_test_example/test01.cpp
+++
@@ -1,8 +0,0 @@
-#include "student_file.h"
-#include "catch.hpp"
-
-TEST_CASE( "Test 01", "Example Project" )
-{
-  int x = 2 + 3;
-  REQUIRE(x == 5);
-}
--- tests/valgrind_test_example/test02.cpp
+++
@@ -1,8 +0,0 @@
-#include "student_file.h"
-#include "catch.hpp"
-
-TEST_CASE( "Test 02", "Example Project" )
-{
-  int *arr = new int[10];
-  delete arr;
-}
